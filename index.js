39b340f0542271e7d5b55270bd22f68263f9da15
Mikhail <mikhailtwere@gmail.com>
2022-06-30 22:57:35 -0600

    commit tree a0e2c4b02de81e7a6490765a6f8cc3747197e14f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+/*const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]*/
+  
+
+function superbowlWin(collection){
+    for (const user of collection){
+        if (user.result === "W") {
+            return (user.year);
+        }
+    }
+}
+
+/*record.find(superbowlWin)*/
